d909f2d3034ed72e194d2b93d17151209c1bfeb0
NEX-DoT <nex.dot2@gmail.com>
2023-09-27 14:08:09 +0330

    card component DONE

--- components/modules/Card.js
+++ components/modules/Card.js
@@ -1,13 +1,26 @@
 import Image from "next/image";
+import Location from "../icons/Location";
+import Dollar from "../icons/Dollar";
+import Link from "next/link";
 const Card = ({data}) => {
-    const {name , price , detail , discount , id } = data;
+    const {name , price , details , discount , id } = data;
     console.log(data);
     return (
-        <div className='w-56 h-72 bg-gray-200 p-1 rounded-lg shadow-lg m-2'>
+        <div className='w-56 h-72 bg-gray-200 p-1 rounded-lg shadow-lg m-2 relative'>
             <img className="rounded" src={`/images/${id}.jpeg`} alt="" />
-            <div>
-                <h3 className="text-lg">{name}</h3>
+            <div className="flex items-center justify-between p-1">
+                <h3 className=" text-lg">{name}</h3>
+                <div className="flex items-center">
+                    <Location/>
+                    <p className="text-sm text-gray-600">{details[0].Cuisine}</p> 
+                </div>
             </div>
+            <div className=" absolute bottom-10 flex">
+                <Dollar/>
+                ${price}
+            </div>
+            <Link href={`/menu/details/${id}`}><button className=" w-11/12 text-white rounded-lg absolute bottom-2 left-1/2 bg-green-700 -translate-x-1/2">More Details</button></Link>
+            
         </div>
     );
 };
