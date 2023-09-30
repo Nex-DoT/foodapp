b61e1055146be75f8d4452272cee12fdfafe8a08
NEX-DoT <nex.dot2@gmail.com>
2023-09-30 12:47:17 +0330

    filltering data

--- components/template/CategoryPage.js
+++ components/template/CategoryPage.js
@@ -1,22 +1,35 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 const CategoryPage = () => {
+    const [query , setQuery] = useState({difficulty:"", time:""});
+    const router = useRouter()
+    const cheangeHandeler = (e)=>{
+        setQuery({
+            ...query,
+            [e.target.name]:e.target.value})
+    }
+    const clickHandeler = ()=>{
+        router.push({pathname:"/category", query})
+    }
     return (
         <div className="m-auto w-2/4">
             <h1 className="text-2xl m-4">Category</h1>
             <br/>
             <hr className=" border-green-600 border-2"/>
             <div className="mt-6 mb-6">
-                <select className="border-2 rounded-md border-green-400 p-1 mt-6 ml-3 outline-none">
+                <select className="border-2 rounded-md border-green-400 p-1 mt-6 ml-3 outline-none" name="difficulty" value={query.difficulty} onChange={cheangeHandeler}>
                     <option value="" key="">Select difficulty</option>
-                    <option value="" key="">Easy</option>
-                    <option value="" key="">Medium</option>
-                    <option value="" key="">Hard</option>
+                    <option value="Easy" key="">Easy</option>
+                    <option value="Medium" key="">Medium</option>
+                    <option value="Hard" key="">Hard</option>
                 </select>
-                <select className="border-2 rounded-md border-green-400 p-1 mt-6 ml-3 outline-none">
+                <select className="border-2 rounded-md border-green-400 p-1 mt-6 ml-3 outline-none" name="time" value={query.time} onChange={cheangeHandeler}>
                     <option value="" key="">Select Time</option>
-                    <option value="" key="">more than 30min</option>
-                    <option value="" key="">less than 30min</option>
+                    <option value="more" key="">more than 30min</option>
+                    <option value="less" key="">less than 30min</option>
                 </select>
-                <button className="border-2 p-1 rounded-md border-green-300 mt-6 ml-3"> Search</button>
+                <button className="border-2 p-1 rounded-md border-green-300 mt-6 ml-3" onClick={clickHandeler}> Search</button>
             </div>
             <img className="m-auto mt-7 mb-7" src="images/search.png" alt="" />
         </div>
--- pages/category/index.js
+++ pages/category/index.js
@@ -10,3 +10,28 @@ const index = () => {
 };
 
 export default index;
+export async function getServerSideProps(context){
+    const {query:{time , difficulty}}= context;
+    console.log(time , difficulty);
+    const res = await fetch("http://localhost:4000/data");
+    const data = await res.json();
+
+    const FiltededData = data.filter((item) => {
+        const difficultyFilter = item.details.filter((detail) => detail.Difficulty && detail.Difficulty === difficulty);
+        console.log(difficultyFilter);
+        const timeFilter = item.details.filter((detail) => {
+            const cookingTime = detail["Cooking Time"] || "";
+            const [Ctime] = cookingTime.split(" ");
+            if(time=== "less" && Ctime && +Ctime < 30){
+                return detail
+            }else if(time=== "more" && Ctime && +Ctime > 30){
+                return detail
+            }
+        })
+        console.log(timeFilter);
+    } )
+
+    return {
+        props: {FiltededData}
+    }
+}
